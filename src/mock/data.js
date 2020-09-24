5dcdb1ce1e88e98673e6a89338a33f8b786c7962
sahilramteke4 <59560511+sahilramteke4@users.noreply.github.com>
2020-09-24 14:56:44 +0530

    Spell corrections

--- src/mock/data.js
+++ src/mock/data.js
@@ -76,7 +76,7 @@ export const projectsData = [
     img: 'funFacts.PNG',
     title: 'Fun facts',
     info:
-      'Fun facrs is an app made with the combination of different APIs. It is fun and cool as it takes the input number and provides some fact related to that number, you can also get location of IIST, you can increase your knowledge of pokemon and having a good laugh with chuck norris jokes is like cherry on the cake.',
+      'Fun facts is an app made with the combination of different APIs. It is fun and cool as it takes the input number and provides some fact related to that number, you can also get location of IIST, you can increase your knowledge of pokemon and having a good laugh with chuck norris jokes is like cherry on the cake.',
     info2:
       'It includes numberfact API, pokeAPI, chuckNorris API and IIST API, made with react and APIs and with some love.',
     url: 'sahilramteke4.github.io/funfacts/',
